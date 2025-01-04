ea3acea7ba21581a31ef2a120175906f39ad67f5
Punny <2472207427@qq.com>
2025-01-04 17:40:55 +0800

    构建项目结构

---
+++ src/test/java/com/punny/commonuser/CommonUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.punny.commonuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
